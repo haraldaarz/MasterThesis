442cafad828dda0c79712def099ad403377b3d00
Haraldaarz <harald@netaarz.com>
2021-12-23 03:28:29 +0100

    Added examples file

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import os
+
+os.system('nmap 127.0.0.1')
+# os.system('masscan -banners')
+
+# Masscan supports json output
+# take results from masscan, and do nmap on the ports
+
